d87341a69d272387f3ed2e8c9a3070083fb33fb6
Brendan Murphy <bjohnmurphy1984@gmail.com>
2017-05-07 11:01:59 -0400

    Updated the win calculation service

--- grails-app/services/seasonoutlook/WinCalcService.groovy
+++ grails-app/services/seasonoutlook/WinCalcService.groovy
@@ -5,17 +5,17 @@ import grails.transaction.Transactional
 @Transactional
 class WinCalcService {
 
-    //calculates the probable winner of the game with using a randomly generated number and a bias
+    //randomly selects the probable winner of the game with using a generated number and a bias
     //toward the team that has a greater winning record vs the opponent
     def setProbableWinner(game) {
         Double d = Math.random()
         if (d < game.homeTeamWinRecord.toFloat().round(2))
         {
-            game.probableWinner = game.awayTeam.name
+            game.probableWinner = game.homeTeam.name
         }
         else if (d <game.awayTeamWinRecord.toFloat().round(2))
         {
-            game.probableWinner = game.homeTeam.name
+            game.probableWinner = game.awayTeam.name
         }
     }
 }
